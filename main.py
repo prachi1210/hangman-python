0f091e41f0b952c34a09e492f4f799191c80e081
Prachi <prachi121096@gmail.com>
2016-06-09 21:39:01 +0530

    Final commit

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os, random, turtle
+stage=[0]
 
-stage=0  
 def get_random_word(file_name):
     total_bytes = os.stat(file_name).st_size 
     random_point = random.randint(0, total_bytes)
     file = open(file_name)
     file.seek(random_point)
-    file.readline() # skip this line to clear the partial line
+    file.readline() 
     return file.readline()
 
-#line= get_random_line(r"C:\hangman-python\wordlist.txt")
+#line= get_random_word(r"C:\hangman-python\wordlist.txt")
 #print(line)
-
 def pt(word, char, i):
     go_to(-5-(len(word)//2*20) - (len(word)//2*10), -150, 0)
     turtle.penup()
@@ -29,9 +28,9 @@ def go_to(x, y, p):
     turtle.setheading(p)
     turtle.pendown()
 
-def hang(stage):
+def hang():
     turtle.speed(2)
-    if stage==0:
+    if stage[0]==0:
         go_to(-300,0,0)
         turtle.forward(600)
         go_to(-100,0, 90)
@@ -40,28 +39,25 @@ def hang(stage):
         turtle.forward(100)
         turtle.right(90)
         turtle.forward(25)
-    elif stage==1:
+    elif stage[0]==1:
         go_to(0, 150, 0)
         turtle.circle(12.5)
-    elif stage==2:
+    elif stage[0]==2:
         go_to(0,150, -90)
         turtle.forward(50)
-    elif stage==3:
+    elif stage[0]==3:
         go_to(0,140, -45)
         turtle.forward(25)
         go_to(0,140, -135)
         turtle.forward(25)
-    elif stage==4:
+    elif stage[0]==4:
         go_to(0,100, -45)
         turtle.forward(25)
         go_to(0,100, -135)
         turtle.forward(25)
-    stage+=1
+    stage[0]+=1
     return 0
 
-#while(stage<5):
-    #hang(stage)
-    #stage+=1
 def spaces(word):
     l=len(word)
     if l %2 !=0:
@@ -79,16 +75,16 @@ def spaces(word):
             turtle.forward(10)
             turtle.pendown()
 
-def mistake(word, char, stage):
+def mistake(word, char):
     go_to(-5-(len(word)//2*20) - (len(word)//2*10), -200, 0)
     turtle.penup()
-    for j in range(stage):
+    for j in range(stage[0]):
         turtle.forward(20)
     turtle.pendown()
     turtle.write(char, align='center', font=("Arial", 8, "normal"))
-    hang(stage)
+    hang()
 
-def play(word, out, stage):
+def play(word, out):
     ch=raw_input('Choose letter to type ')
     key=''
     if ch in word:
@@ -100,24 +96,22 @@ def play(word, out, stage):
                 key+=out[i]
         return key
     else:
-        mistake(word, ch, stage)
+        mistake(word, ch)
         return out
 
 def main():
     word=get_random_word(r"C:\Udacity\hangman-python\wordlist.txt").strip('\n').lower()
     print(word)
-    stage=0
     #print(len(word))
     print("We have a chosen a "+str(len(word))+" lettered word")
     spaces(word)
     out=' '
     for i in range(len(word)):
         out+='_'
-    while out != word and stage<=4:
+    while out != word and stage[0] <=4:
         print(out)
-        out=play(word, out, stage)
-        stage=stage+1
-    if stage > 4:
+        out=play(word, out)
+    if stage[0] > 4:
         print('Oops. You died')
         turtle.bgcolor('red')
         turtle.exitonclick()
